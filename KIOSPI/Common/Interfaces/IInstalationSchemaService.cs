635627c5daa6ae457b64e2fbe71d10cb7c52eaf4
Patryk Kozarski <Patryknerio2@gmail.com>
2022-06-09 17:56:35 +0200

    walidacja & instalator

---
+++ KIOSPI/Common/Interfaces/IInstalationSchemaService.cs
@@ -0,0 +1,9 @@
+﻿using Common.Models;
+
+namespace Common.Interfaces
+{
+    public interface IInstalationSchemaService
+    {
+        public InstallationSchema ReadInstallationSchema(string filePath);
+    }
+}
---
+++ KIOSPI/Common/Services/InstalationSchemaService.cs
@@ -0,0 +1,14 @@
+﻿using Common.Interfaces;
+using Common.Models;
+using Newtonsoft.Json;
+
+namespace Common.Services
+{
+    internal class InstalationSchemaService : IInstalationSchemaService
+    {
+        public InstallationSchema ReadInstallationSchema(string filePath)
+        {
+            return JsonConvert.DeserializeObject<InstallationSchema>(filePath);
+        }
+    }
+}
