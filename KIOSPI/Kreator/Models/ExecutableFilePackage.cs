8996e9b83a6ab7aaa74a683803657877e6dc31c9
Patryk Kozarski <patryk.kozarski@microsoft.wsei.edu.pl>
2022-07-08 19:37:27 +0200

    instalator, logger, sprawdzanie rejestru

---
+++ KIOSPI/Kreator/Models/ExecutableFilePackage.cs
@@ -0,0 +1,14 @@
+﻿using Common.Models;
+
+namespace Kreator.Models
+{
+    internal class ExecutableFilePackage
+    {
+        public string FilePath { get; set; }
+
+        public ExecutableFilePackage(ExecutableFileSchema schema)
+        {
+            FilePath = schema.FilePath;
+        }
+    }
+}
---
+++ KIOSPI/Kreator/Services/InstalatorService.cs
@@ -0,0 +1,21 @@
+﻿using Kreator.Interfaces;
+using Kreator.Models;
+
+namespace Kreator.Services
+{
+    internal class InstalatorService : IInstalatorService
+    {
+        //todo logger
+        public void CreateInstalator(string packagePath, string schemaPath)
+        {
+            //todo zip
+            throw new NotImplementedException();
+        }
+
+        public void CreateInstallationPackage(List<ExecutableFilePackage> files, string outputPath)
+        {
+            //todo instalator
+            throw new NotImplementedException();
+        }
+    }
+}
