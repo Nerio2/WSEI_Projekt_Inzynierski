7848b7599e96f788217046f09528008615100304
Patryk Kozarski <patryk.kozarski@microsoft.wsei.edu.pl>
2022-07-08 19:37:27 +0200

    budowanie instalatora przez kreator WIP

---
+++ KIOSPI/Kreator/Interfaces/IInstalatorService.cs
@@ -0,0 +1,12 @@
+﻿using Common.Models;
+using Kreator.Models;
+
+namespace Kreator.Interfaces
+{
+    internal interface IInstalatorService
+    {
+        void CreateInstallationPackage(List<ExecutableFilePackage> files, string outputPath);
+
+        void CreateInstalator(string packagePath, string schemaPath);
+    }
+}
